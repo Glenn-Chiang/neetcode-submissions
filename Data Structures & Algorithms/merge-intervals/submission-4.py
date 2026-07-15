e0fc2f333cdcee3d699cde52d228977f43dc35f4
Glenn <122365492+Glenn-Chiang@users.noreply.github.com>
2026-07-15 18:04:19 +0800

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # Sort intervals by start
+        intervals.sort(key=lambda x: x[0])
+
+        res = []
+        prev_interval = intervals[0]
+        for interval in intervals:
+            # If current interval overlaps with previous interval, 
+            # merge current interval into previous interval
+            if interval[0] <= prev_interval[1]:
+                prev_interval[1] = max(prev_interval[1], interval[1])
+            
+            # If current interval does not overlap with previous interval,
+            # add the previous interval to result and start a new interval
+            else:
+                res.append(prev_interval.copy())
+                prev_interval = interval
+        
+        res.append(prev_interval)
+        return res
